0e71686080cb5fb88e7b16d353ad1cceae6e271a
om shingare <omshingare120@gmail.com>
2024-02-08 03:26:30 +0530

    "code added"

--- codepushauto.py
+++ codepushauto.py
@@ -3,7 +3,5 @@ import subprocess
 subprocess.call(["git", "add", "*"])
 subprocess.call(["git", "commit", "-m", '"code added"'])
 subprocess.call(["git", "push"])
-# subprocess.call(["clear"])
 print("\033c", end='')
 print("Code Uploded")
-# Hello this is code
