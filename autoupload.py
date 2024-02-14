3c8ca69586c166fa8c409b6071b8f8fa32822b1b
om shingare <omshingare120@gmail.com>
2024-02-15 01:11:52 +0530

    "code added"

--- autoupload.py
+++
@@ -1,6 +0,0 @@
-import time
-import subprocess
-
-while True:
-    subprocess.run(["python", "codepushauto.py"])
-    time.sleep(300)  # 300 seconds = 5 minutes
--- codepushauto.py
+++ codepushauto.py
@@ -1,7 +1,13 @@
 import subprocess
+import time
 
-subprocess.call(["git", "add", "*"])
-subprocess.call(["git", "commit", "-m", '"code added"'])
-subprocess.call(["git", "push"])
-print("\033c", end='')
-print("Code Uploded")
+def upload():
+    subprocess.call(["git", "add", "*"])
+    subprocess.call(["git", "commit", "-m", '"code added"'])
+    subprocess.call(["git", "push"])
+    print("\033c", end='')
+    print("Code Uploded")
+
+while True:
+    upload()
+    time.sleep(300)
